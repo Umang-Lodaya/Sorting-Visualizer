6673728000eeab1233fd3968591affedb35779da
UMANG KIRIT LODAYA <lodayaumang71@gmail.com>
2023-10-22 19:05:51 +0530

    Added Quick and Merge

--- main.py
+++ main.py
@@ -14,93 +14,154 @@ st.markdown(
 )
 st.markdown("")
 
-
-def getChart(data):
-    return (
-        alt.Chart(data)
-        .mark_bar()
-        .encode(
-            alt.X("index"),
-            alt.Y("value"),
-            alt.Color("colour", legend=None),
-            tooltip=["value"],
-        )
-        .interactive()
-    )
-
-
 print("\n*****************")
 
-N_COL, S_COL = st.columns(2, gap="large")
+ALGORITHMS = ["Bubble Sort", "Insertion Sort", "Selection Sort", "Merge Sort", "Quick Sort"]
+# ALGORITHMS.sort()
+
+N_COL, T_COL = st.columns([3, 2], gap="large")
 with N_COL:
-    N = st.slider("Enter the Range of Numbers", 10, 100, 50, 5)
-with S_COL:
-    SPEED = st.slider("Speed: ", 10, 100, 10)
-
-df = pd.DataFrame([random.randint(1, 1000) for _ in range(N)], columns=["value"])
-colours = [i for i in range(0xF0F00A, 0xF0F0FF)]
-df["colour"] = df.value.apply(lambda x: colours[x // 10])
-df.reset_index(inplace=True)
-
-ALGORITHMS = ["Bubble Sort", "Insertion Sort", "Selection Sort"]
-T_COL, B_COL = st.columns(2, gap="large")
+    N = st.slider("Number of Elements: ", 10, 100, 50, 5)
+    arr = [random.randint(1, 1000) for _ in range(N)]
 with T_COL:
     ALGO = st.selectbox("Sorting Algorithm", ALGORITHMS)
-with B_COL:
+
+A_COL, T_COL = st.columns([2.5, 2], gap="large")
+button = False
+with A_COL:
     st.markdown("####")
-    button = st.button("SORT!")
+    if st.button("RANDOMIZE!", use_container_width=True):
+        arr = [random.randint(1, 1000) for _ in range(N)]
+with T_COL:
+    SPEED = 0.1 / st.slider("Speed: ", 1, 10, 1)
+
+button = False
+button = st.button("SORT!", use_container_width=True)
+st.markdown("###")
 
 plot_spot = st.empty()
+with plot_spot:
+    st.bar_chart(arr)
 
 if button:
     if ALGO == ALGORITHMS[0]:
         swapped = False
         for i in range(N - 1):
             for j in range(0, N - i - 1):
-                arr = list(df.value)
                 if arr[j] > arr[j + 1]:
                     swapped = True
                     arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                    df.value = arr
                     with plot_spot:
-                        df["colour"] = df.value.apply(lambda x: colours[x // 10])
-                        st.altair_chart(getChart(df), use_container_width=True)
+                        st.bar_chart(arr)
 
-                time.sleep(10 / SPEED)
+                time.sleep(SPEED)
 
             if not swapped:
                 break
 
     elif ALGO == ALGORITHMS[1]:
-        arr = list(df.value)
         for i in range(1, N):
             while i > 0 and arr[i - 1] > arr[i]:
-                arr = list(df.value)
                 arr[i - 1], arr[i] = arr[i], arr[i - 1]
                 i -= 1
-                df.value = arr
                 with plot_spot:
-                    df["colour"] = df.value.apply(lambda x: colours[x // 10])
-                    st.altair_chart(getChart(df), use_container_width=True)
+                    st.bar_chart(arr)
 
-                time.sleep(10 / SPEED)
+                time.sleep(SPEED)
 
     elif ALGO == ALGORITHMS[2]:
-        for i in range(N):
-            arr = list(df.value)
+        for i in range(N - 1):
             min_i = i
             for j in range(i + 1, N):
                 if arr[j] < arr[min_i]:
                     min_i = j
-            
-            arr[min_i], arr[i] = arr[i], arr[min_i]
-            df.value = arr
-            with plot_spot:
-                df["colour"] = df.value.apply(lambda x: colours[x // 10])
-                st.altair_chart(getChart(df), use_container_width=True)
 
-            time.sleep(10 / SPEED)
+            arr[min_i], arr[i] = arr[i], arr[min_i]
+            with plot_spot:
+                st.bar_chart(arr)
+
+            time.sleep(SPEED)
+
+    elif ALGO == ALGORITHMS[3]:
+
+        def merge_sort(array, left_index, right_index):
+            if left_index >= right_index:
+                return
+            middle = (left_index + right_index) // 2
+            merge_sort(array, left_index, middle)
+            merge_sort(array, middle + 1, right_index)
+            merge(array, left_index, right_index, middle)
+
+
+        def merge(array, left_index, right_index, middle):
+            left_sublist = array[left_index : middle + 1]
+            right_sublist = array[middle + 1 : right_index + 1]
+            left_sublist_index = 0
+            right_sublist_index = 0
+            sorted_index = left_index
+            while left_sublist_index < len(left_sublist) and right_sublist_index < len(right_sublist):
+                if left_sublist[left_sublist_index] <= right_sublist[right_sublist_index]:
+                    array[sorted_index] = left_sublist[left_sublist_index]
+                    left_sublist_index = left_sublist_index + 1
+                else:
+                    array[sorted_index] = right_sublist[right_sublist_index]
+                    right_sublist_index = right_sublist_index + 1
+                
+                with plot_spot:
+                    st.bar_chart(array)
+                time.sleep(SPEED)
+
+                sorted_index = sorted_index + 1
+            while left_sublist_index < len(left_sublist):
+                array[sorted_index] = left_sublist[left_sublist_index]
+                left_sublist_index = left_sublist_index + 1
+                sorted_index = sorted_index + 1
+                with plot_spot:
+                    st.bar_chart(array)
+                time.sleep(SPEED)
+
+            while right_sublist_index < len(right_sublist):
+                array[sorted_index] = right_sublist[right_sublist_index]
+                right_sublist_index = right_sublist_index + 1
+                sorted_index = sorted_index + 1
+                with plot_spot:
+                    st.bar_chart(array)
+                time.sleep(SPEED)
+        
+        merge_sort(arr, 0, N - 1)
+
+
+    elif ALGO == ALGORITHMS[4]:
+        def partition(array, low, high):
+            pivot = array[high]
+            i = low - 1
+
+            for j in range(low, high):
+                if array[j] <= pivot:
+                    i = i + 1
+                    array[i], array[j] = array[j], array[i]
+                    with plot_spot:
+                        st.bar_chart(array)
+                    time.sleep(SPEED)
+
+            array[i + 1], array[high] = array[high], array[i + 1]
+            with plot_spot:
+                st.bar_chart(array)
+            time.sleep(SPEED)
+
+            return i + 1
+
+        def quickSort(array, low, high):
+            if low < high:
+                pi = partition(array, low, high)
+                quickSort(array, low, pi - 1)
+                quickSort(array, pi + 1, high)
+        
+        quickSort(arr, 0, N - 1)
+
+    with plot_spot:
+        st.bar_chart(arr, color="#00FF00")
+        time.sleep(0.5)
 
 with plot_spot:
-    df["colour"] = df.value.apply(lambda x: colours[x // 10])
-    st.altair_chart(getChart(df), use_container_width=True)
+    st.bar_chart(arr)
